5ff3b361366bb90bea1c3b52173a87458194b330
Daniel Schepers <schepedw@rose-hulman.edu>
2013-09-29 22:24:42 -0400

    Initial GUI skeleton
    Mostly just a guess

---
+++ src/gui/StatusPanel.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author schepedw.
+ *         Created Sep 29, 2013.
+ */
+public class StatusPanel extends JPanel{
+
+}
