01a7f5887107a25b264c5a1a7dfc73454cc1a996
Daniel Schepers <schepedw@rose-hulman.edu>
2013-10-01 21:52:29 -0400

    Execution panel works

--- src/gui/ExecutionPanel.java
+++ src/gui/ExecutionPanel.java
@@ -1,36 +1,13 @@
 package gui;
 
-import java.awt.BorderLayout;
-import java.io.PrintStream;
-
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
-public class ExecutionPanel extends JPanel {
+/**
+ * TODO Put here a description of what this class does.
+ *
+ * @author schepedw.
+ *         Created Sep 29, 2013.
+ */
+public class ExecutionPanel extends JPanel{
 
-	public ExecutionPanel() {
-		add(new JLabel("Console"));
-
-		JTextArea ta = new JTextArea();
-		Console output = new Console(ta, 60);
-		PrintStream ps = new PrintStream(output);
-		System.setOut(ps);
-		System.setErr(ps);
-
-		add(new JScrollPane(ta));
-
-		setVisible(true);
-
-		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException exception) {
-				// TODO Auto-generated catch-block stub.
-				exception.printStackTrace();
-			}
-		}
-	}
 }
